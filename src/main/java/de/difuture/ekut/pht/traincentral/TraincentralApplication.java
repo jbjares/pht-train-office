964869a9b2b33f5925a670d1054016ca1a23c524
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-03-07 14:24:43 +0100

    [TEST] Example message source for testing

--- src/main/java/de/difuture/ekut/pht/traincentral/TraincentralApplication.java
+++ src/main/java/de/difuture/ekut/pht/traincentral/TraincentralApplication.java
@@ -1,12 +1,29 @@
 package de.difuture.ekut.pht.traincentral;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.core.MessageSource;
+import org.springframework.messaging.support.GenericMessage;
 
 @SpringBootApplication
+@EnableBinding(Source.class)
 public class TraincentralApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TraincentralApplication.class, args);
 	}
+	
+	@Bean
+    @InboundChannelAdapter(value = Source.OUTPUT)
+    public MessageSource<String> timerMessageSource() {
+		
+        return () -> new GenericMessage<>(new SimpleDateFormat().format(new Date()));
+    }
 }
